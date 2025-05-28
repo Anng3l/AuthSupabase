89245ee2c1bb96e4bd872de892502c69c33edf3e
Angel Maldonado <151583275+Anng3l@users.noreply.github.com>
2025-05-28 15:27:48 -0500

    asd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  supabaseUrl: "https://hcplyvetveqursvpxgsy.supabase.co",
-  supabasekey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhjcGx5dmV0dmVxdXJzdnB4Z3N5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0NTkwMzgsImV4cCI6MjA2NDAzNTAzOH0.N8Fyuukf6dNI9Tfihck-FNFPMqoy8hvRtrkcELApb-4"
+  supabaseUrl: "",
+  supabasekey: ""
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  supabaseUrl: "https://hcplyvetveqursvpxgsy.supabase.co",
-  supabasekey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhjcGx5dmV0dmVxdXJzdnB4Z3N5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0NTkwMzgsImV4cCI6MjA2NDAzNTAzOH0.N8Fyuukf6dNI9Tfihck-FNFPMqoy8hvRtrkcELApb-4"
+  supabaseUrl: "",
+  supabasekey: ""
 };
 
 /*
